153e58ab90b2f1c358c3f0de98adbf83459fd689
Vlad Holubiev <3817380+vladholubiev@users.noreply.github.com>
2025-08-03 22:22:26 +0200

    test: expect new structure

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,24 @@
 import {fetchPageOfNews, getTotalPagesCount} from './index';
 
+function extractKeysFlat(obj: Record<string, any>, prefix = ''): string[] {
+  const keys: string[] = [];
+
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      const fullKey = prefix ? `${prefix}.${key}` : key;
+      const value = obj[key];
+
+      if (value !== null && typeof value === 'object' && !Array.isArray(value)) {
+        keys.push(...extractKeysFlat(value, fullKey));
+      } else {
+        keys.push(fullKey);
+      }
+    }
+  }
+
+  return keys.sort();
+}
+
 it('should return page of news in the expected format', async () => {
   const pageOfNews = await fetchPageOfNews({year: 2024, pageNumber: 8, pageSize: 3});
 
@@ -7,83 +26,78 @@ it('should return page of news in the expected format', async () => {
     [
       {
         "additionalFields": {
-          "contentType": "Launch Announcement",
-          "headline": "Amazon EC2 C6id instances are now available in Canada (Central) region",
-          "headlineUrl": "/about-aws/whats-new/2024/06/amazon-ec2-instances-canada-central-region/",
+          "headline": "AWS Neuron introduces support for Trainium2 and NxD Inference",
+          "headlineUrl": "/about-aws/whats-new/2024/12/aws-neuron-trainium2-nxd-inference",
           "nofollow": "0",
           "noindex": "0",
           "nosnippet": "0",
-          "postBody": "<p>Starting today, <a href="https://aws.amazon.com/ec2/instance-types/c6i/" target="_blank">Amazon Elastic Compute Cloud (Amazon EC2) C6id instances</a> are available in Canada (Central) Region. These instances are powered by 3rd generation Intel Xeon Scalable Ice Lake processors with an all-core turbo frequency of 3.5 GHz and up to 7.6 TB of local NVMe-based SSD block-level storage. C6id instances are built on <a href="https://aws.amazon.com/ec2/nitro/" target="_blank">AWS Nitro System</a>, a combination of dedicated hardware and lightweight hypervisor, which delivers practically all of the compute and memory resources of the host hardware to your instances for better overall performance and security. Customers can take advantage of access to high-speed, low-latency local storage for compute-intensive workloads, such as batch processing, distributed analytics, high performance computing (HPC), ad serving, highly scalable multiplayer gaming, and video encoding.<br />
+          "postBody": "<p>Today, AWS announces the release of <a href="https://awsdocs-neuron.readthedocs-hosted.com/en/latest/release-notes/index.html" target="_blank">Neuron 2.21</a>, introducing support for AWS Trainium2 chips and Amazon EC2 Trn2 instances, including the trn2.48xlarge instance type and Trn2 UltraServer. This release also adds support for PyTorch 2.5 and introduces NxD Inference and Neuron Profiler 2.0 (beta). <a href="https://awsdocs-neuron.readthedocs-hosted.com/en/latest/libraries/nxd-inference/index.html#nxdi-index" target="_blank">NxD Inference</a>, is a new PyTorch-based library integrated with vLLM, simplifies the deployment of large language and multi-modality models and enables PyTorch model onboarding with minimal code changes, and Neuron Profiler 2.0 (beta), is new profiler that enhances capabilities and usability, including support for distributed workloads.<br />
     <br />
-    These instances are generally available today in the US (Ohio, N.Virginia, Oregon), Canada (Calgary, Central), Asia Pacific (Tokyo, Sydney, Seoul, Singapore), Europe (Ireland, Frankfurt, London), Israel (Tel Aviv), and AWS GovCloud (US-West) <a href="https://aws.amazon.com/about-aws/global-infrastructure/regional-product-services/" target="_blank">Regions</a>.<br />
+    Neuron 2.21 also introduces Llama 3.1 405B model inference support using NxD Inference on a single trn2.48xlarge instance. The release updates Deep Learning Containers (DLCs) and Deep Learning AMIs (DLAMIs), and adds support for various model architectures, including Llama 3.2, Llama 3.3, and Mixture-of-Experts (MoE) models. New inference features include FP8 weight quantization and flash decoding for speculative decoding in Transformers NeuronX (TNx). Additionally, new training examples and features have been added, such as support for HuggingFace Llama 3/3.1 70B on Trn2 instances and DPO support for post-training model alignment.<br />
     <br />
-    Customers can purchase the new instances via Savings Plans, Reserved, On-Demand, and Spot instances. To learn more, see <a href="https://aws.amazon.com/ec2/instance-types/c6i/" target="_blank">Amazon C6id instances</a>. To get started, visit <a href="https://aws.amazon.com/cli/" target="_blank">AWS Command Line Interface (CLI)</a>, and <a href="http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html" target="_blank">AWS SDKs</a>.</p>
+    AWS Neuron SDK supports training and deploying models on Trn1, Trn2, and Inf2 instances, available in AWS Regions as On-Demand Instances, Reserved Instances, Spot Instances, or part of Savings Plan.<br />
+    <br />
+    For a full list of new features and enhancements in Neuron 2.21 and to get started with Neuron, see:</p>
+
+    <ul>
+    	<li><a href="https://awsdocs-neuron.readthedocs-hosted.com/en/latest/" target="_blank">AWS Neuron</a></li>
+    	<li><a href="https://aws.amazon.com/ec2/instance-types/trn2/" target="_blank">Trn2 Instances</a></li>
+    	<li><a href="https://aws.amazon.com/ec2/instance-types/trn1/" target="_blank">Trn1 Instances</a></li>
+    	<li><a href="https://aws.amazon.com/ec2/instance-types/inf2/" target="_blank">Inf2 Instances</a></li>
+    </ul>
     ",
-          "postDateTime": "2024-06-05T10:00:00-07:00",
+          "postDateTime": "2024-12-23T13:00:00-05:00",
         },
-        "author": "gilsimm",
-        "createdBy": "gilsimm",
-        "dateCreated": "2024-05-20T20:46:07+0000",
-        "dateUpdated": "2024-06-05T17:08:25+0000",
+        "dateCreated": "2024-12-20T18:47:31+0000",
+        "dateUpdated": "2024-12-23T18:51:04+0000",
         "directoryId": "whats-new-v2",
-        "id": "whats-new-v2#p128837820",
-        "lastUpdatedBy": "gilsimm",
+        "id": "whats-new-v2#p180320882",
         "locale": "en_US",
-        "name": "wTbxfX4uXcLEx4G5E4tYvP",
+        "name": "uRYJByhWKzVK6EHcA2jTUD",
       },
       {
         "additionalFields": {
-          "contentType": "Launch Announcement",
-          "headline": "AWS HealthImaging now publishes events to Amazon EventBridge",
-          "headlineUrl": "/about-aws/whats-new/2024/06/aws-healthimaging-publishes-events-eventbridge",
+          "headline": "AWS Systems Manager for SAP now supports SAP NetWeaver based ABAP applications deployed in distributed and HA topologies",
+          "headlineUrl": "/about-aws/whats-new/2024/12/aws-systems-manager-sap-netweaver-based-abap-applications-deployed-distributed-ha-topologies",
           "nofollow": "0",
           "noindex": "0",
           "nosnippet": "0",
-          "postBody": "<p>AWS HealthImaging now supports event-driven architectures by sending event notifications to Amazon EventBridge. By subscribing to HealthImaging events in EventBridge, you can automatically kick-off application workflows such as image quality assessment or de-identification based upon changes to resources in the data store. With EventBridge, developers can take advantage of a serverless event bus to easily connect and route events between many AWS services and third-party applications. Developers working with HealthImaging can now receive state changes for asynchronous tasks, such as DICOM import jobs and image set copy and update operations. Events are delivered to EventBridge in near real-time, and developers can write simple rules to listen for specific events.<br />
+          "postBody": "<p>AWS Systems Manager for SAP includes capabilities to now support registration of SAP NetWeaver ABAP-based applications, deployed in distributed and Highly Available (HA) topologies. This enhancement covers a wide range of applications, including S/4HANA and BW/4HANA, and extends support to additional application servers and web dispatcher components. With this launch, customers can automate start and stop operations for ABAP based SAP applications running on distributed and HA architectures. This new functionality simplifies the management of complex SAP landscapes, improves operational efficiency through automation, and provides enhanced support for high-availability configurations.<br />
     <br />
-    AWS HealthImaging is a HIPAA-eligible service that empowers healthcare providers and their software partners to store, analyze, and share medical images at petabyte scale. With AWS HealthImaging, you can run your medical imaging applications at scale from a single, authoritative copy of each medical image in the cloud, while reducing total cost of ownership.<br />
+    AWS Systems Manager for SAP is an automation capability to manage and operate your SAP applications on AWS. It provides a seamless integration between AWS services and SAP applications running on AWS. Registering SAP applications to AWS Systems Manager for SAP allows automation such as enabling backup, start/stop for the applications and associated EC2 instances. In addition, as part of the SSM Application Manager console integration, customers can get details of the resources being consumed by the SAP applications such as EC2 instances, EBS volumes and the associated costs incurred at an application level.<br />
     <br />
-    AWS HealthImaging is generally available in the following <a href="https://aws.amazon.com/about-aws/global-infrastructure/regional-product-services/" target="_blank">AWS Regions</a>: US East (N. Virginia), US West (Oregon), Asia Pacific (Sydney), and Europe (Ireland).<br />
-    <br />
-    To learn more, visit <a href="https://aws.amazon.com/healthimaging/" target="_blank">AWS HealthImaging</a>.<br />
-    &nbsp;</p>
+    AWS Systems Manager for SAP is available in all commercial regions. To learn about the key features, check out the <a href="https://docs.aws.amazon.com/ssm-sap/latest/userguide/what-is-ssm-for-sap.html" target="_blank">product documentation page</a>. To learn why thousands of SAP customers trust AWS, visit the <a href="https://aws.amazon.com/sap/" target="_blank">SAP on AWS page</a>.</p>
     ",
-          "postDateTime": "2024-06-05T10:00:00-07:00",
+          "postDateTime": "2024-12-23T13:00:00-05:00",
         },
-        "author": "gilsimm",
-        "createdBy": "gilsimm",
-        "dateCreated": "2024-06-03T16:27:14+0000",
-        "dateUpdated": "2024-06-05T19:38:28+0000",
+        "dateCreated": "2024-12-19T15:52:55+0000",
+        "dateUpdated": "2024-12-23T18:10:44+0000",
         "directoryId": "whats-new-v2",
-        "id": "whats-new-v2#p129938170",
-        "lastUpdatedBy": "gilsimm",
+        "id": "whats-new-v2#p176546368",
         "locale": "en_US",
-        "name": "launch-bvxH1u7XMKqDmrVxkVXUMZ",
+        "name": "1fhCPRXfPXAD4ZZrFt5tdm",
       },
       {
         "additionalFields": {
-          "contentType": "launch announcement",
-          "headline": "Introducing Amazon EMR Serverless Streaming jobs for continuous processing on streaming data",
-          "headlineUrl": "/about-aws/whats-new/2024/06/amazon-emr-serverless-streaming-jobs-continuous-processing",
+          "headline": "AWS announces notification actions in the AWS Console Mobile App for iOS",
+          "headlineUrl": "/about-aws/whats-new/2024/12/aws-notification-actions-aws-console-mobile-app-ios",
           "nofollow": "0",
           "noindex": "0",
           "nosnippet": "0",
-          "postBody": "<p><a href="https://aws.amazon.com/emr/serverless/" target="_blank">Amazon EMR Serverless</a> is a serverless option in Amazon EMR that makes it simple for data engineers and data scientists to run open-source big data analytics frameworks without configuring, managing, and scaling clusters or servers. We are excited to announce a new streaming job mode on Amazon EMR Serverless, enabling you to continuously analyze and process streaming data.<br />
-    Streaming has become vital for businesses to gain continuous insights from data sources like sensors, IoT devices, and web logs. However, processing streaming data can be challenging due to requirements such as high availability, resilience to failures, and integration with streaming services. Amazon EMR Serverless Streaming jobs has built-in features to addresses these challenges. It offers high availability through multi-AZ (Availability Zone) resiliency by automatically failing over to healthy AZs. It also offers increased resiliency through automatic job retries on failures and log management features like log rotation and compaction, preventing the accumulation of log files that might lead to job failures. In addition, Amazon EMR Serverless Streaming jobs support processing data from streaming services like self-managed Apache Kafka clusters, Amazon Managed Streaming for Apache Kafka, and now is integrated with Amazon Kinesis Data Streams using a new built-in <a href="https://docs.aws.amazon.com/emr/latest/ReleaseGuide/emr-spark-structured-streaming-kinesis.html" target="_blank">Amazon Kinesis Data Streams Connector</a>, making it easier to build end-to-end streaming pipelines.</p>
+          "postBody": "<p>Today, Amazon Web Services (AWS) is announcing the general availability of notification actions in the AWS Console Mobile Application for iOS. Notification action buttons are displayed on the notification details screen when you receive a push notification from <a href="https://aws.amazon.com/notifications/" target="_blank">AWS User Notifications</a> on your mobile device. AWS customers can take actions like view logs, start, stop, or reboot an EC2 instance on an event notification to shorten incident response diagnosis and resolution times.<br />
+    <br />
+    With notification actions, you can quickly run common DevOps tasks based on your IAM permissions using notification action buttons. Notification actions are displayed automatically on notifications based on the state of the AWS resource. Using notification actions for event notifications, you can restart an EC2 instance the moment you receive an EC2 instance terminated push notification on your mobile device or get more information about an event notification without needing to return to your computer.<br />
+    <br />
+    The Console Mobile App lets users view and manage a select set of resources to stay informed and connected with their AWS resources while on-the-go. Visit the <a href="https://aws.amazon.com/console/mobile/" target="_blank">product page</a> for more information about the Console Mobile Application.</p>
     ",
-          "postDateTime": "2024-06-04T16:00:00-04:00",
-          "regionalAvailability": "<p>Amazon EMR Serverless Streaming jobs is generally available on EMR release versions 7.1.0 and later and in the following AWS Regions: US East (N. Virginia, Ohio), US West (Oregon), Europe (Stockholm, Paris, Frankfurt, Ireland, London), South America (S&atilde;o Paulo) and Asia Pacific (Tokyo, Seoul, Singapore, Mumbai, Sydney). To get started, visit the<a href="https://docs.aws.amazon.com/emr/latest/EMR-Serverless-UserGuide/jobs-streaming.html" target="_blank"> Amazon EMR Serverless Streaming jobs</a> page in the Amazon EMR Serverless User Guide.</p>
-    ",
+          "postDateTime": "2024-12-23T13:00:00-05:00",
         },
-        "author": "morrette",
-        "createdBy": "morrette",
-        "dateCreated": "2024-06-03T17:12:12+0000",
-        "dateUpdated": "2024-06-04T20:06:40+0000",
+        "dateCreated": "2024-12-19T19:48:55+0000",
+        "dateUpdated": "2024-12-23T18:18:05+0000",
         "directoryId": "whats-new-v2",
-        "id": "whats-new-v2#v1342859142",
-        "lastUpdatedBy": "morrette",
+        "id": "whats-new-v2#p165712166",
         "locale": "en_US",
-        "name": "bKiV4RSuZEbNU5ocXEqXhM",
+        "name": "pGmNYYJvpLwUEM5JoeUF8b",
       },
     ]
   `);
@@ -93,4 +107,33 @@ it('should return total pages count', async () => {
   const totalPagesCount = await getTotalPagesCount({year: 2024, pageSize: 100});
 
   expect(totalPagesCount).toBeGreaterThan(8);
+});
+
+it('should return expected object structure keys (shows removed keys in failure)', async () => {
+  const pageOfNews = await fetchPageOfNews({year: 2025, pageNumber: 1, pageSize: 3});
+  const firstItem = pageOfNews.items[0]?.item;
+
+  if (!firstItem) {
+    throw new Error('No items returned from API');
+  }
+
+  const keys = extractKeysFlat(firstItem);
+
+  expect(keys).toMatchInlineSnapshot(`
+    [
+      "additionalFields.headline",
+      "additionalFields.headlineUrl",
+      "additionalFields.nofollow",
+      "additionalFields.noindex",
+      "additionalFields.nosnippet",
+      "additionalFields.postBody",
+      "additionalFields.postDateTime",
+      "dateCreated",
+      "dateUpdated",
+      "directoryId",
+      "id",
+      "locale",
+      "name",
+    ]
+  `);
 });
